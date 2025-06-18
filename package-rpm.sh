e41fa4858e37cdea156d42b5b7046ef0a000ddb7
aman-lnu <aman1+github@microsoft.com>
2025-06-18 10:10:48 +0000

    added script for building rpm and deb

---
+++ package-rpm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+sudo dnf -y install rpm-build rpmdevtools autoconf libtool make gcc gcc-c++ python3-devel libcurl-devel krb5-devel chrpath git
+dnf clean all
+
+rpmdev-setuptree ~
+# TODO: change the version number here 
+git archive --format=tar --prefix=azfilesauth-1.0/ HEAD -- . ':!debian' | gzip > ~/rpmbuild/SOURCES/azfilesauth-1.0.tar.gz
+cp rpm.spec ~/rpmbuild/SPECS/
+rpmbuild -ba ~/rpmbuild/SPECS/rpm.spec
+
+cp ~/rpmbuild/RPMS/x86_64/azfilesauth*.rpm PACKAGES/rpm/
