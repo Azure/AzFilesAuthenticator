921ca4958b7f17571aa234b6d383bdcb1a959712
aman-lnu <aman1+github@microsoft.com>
2025-08-06 01:32:36 +0530

    Update package-rpm.sh

--- package-rpm.sh
+++ package-rpm.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -x
 
-sudo dnf -y install rpm-build rpmdevtools autoconf libtool make gcc gcc-c++ python3-devel libcurl-devel krb5-devel chrpath git
-dnf clean all
+# Check if the system is Debian/apt-based
+if [ -x "$(command -v apt-get)" ] || [ -f /etc/debian_version ]; then
+    # Debian/Ubuntu system
+    sudo apt-get install rpm -y
+else
+    # Assuming it's an RPM-based system (Fedora, CentOS, RHEL, etc.)
+    sudo dnf -y install rpm-build rpmdevtools autoconf libtool make gcc gcc-c++ python3-devel libcurl-devel krb5-devel chrpath git
+    dnf clean all
+fi
 
 rpmdev-setuptree ~
 # TODO: change the version number here 
