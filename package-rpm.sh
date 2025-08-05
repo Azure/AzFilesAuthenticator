00c29552570599a63b9743394f053f952d83a002
aman-lnu <aman1+github@microsoft.com>
2025-08-06 01:48:04 +0530

    Update package-rpm.sh

--- package-rpm.sh
+++ package-rpm.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 set -x
 
+setup_rpmbuild_tree() {
+    local topdir="${HOME}/rpmbuild"
+    local dirs=("BUILD" "RPMS" "SOURCES" "SPECS" "SRPMS")
+
+    echo "Creating RPM build tree in: $topdir"
+
+    for dir in "${dirs[@]}"; do
+        mkdir -p "${topdir}/${dir}"
+    done
+
+    echo "%_topdir ${topdir}" > "${HOME}/.rpmmacros"
+    echo "RPM build tree created successfully."
+    echo "~/.rpmmacros file set with %_topdir ${topdir}"
+}
+
 # Check if the system is Debian/apt-based
 if [ -x "$(command -v apt-get)" ] || [ -f /etc/debian_version ]; then
     # Debian/Ubuntu system
     sudo apt-get install rpm -y
+    setup_rpmbuild_tree
 else
     # Assuming it's an RPM-based system (Fedora, CentOS, RHEL, etc.)
     sudo dnf -y install rpm-build rpmdevtools autoconf libtool make gcc gcc-c++ python3-devel libcurl-devel krb5-devel chrpath git
     dnf clean all
+    rpmdev-setuptree ~
 fi
 
-rpmdev-setuptree ~
+
 # TODO: change the version number here 
 git archive --format=tar --prefix=azfilesauth-1.0/ HEAD -- . ':!debian' | gzip > ~/rpmbuild/SOURCES/azfilesauth-1.0.tar.gz
 cp rpm.spec ~/rpmbuild/SPECS/
