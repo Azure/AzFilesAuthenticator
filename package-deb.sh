478280764e6d7669ed1413aa2c53b757c3aadc0e
aman-lnu <aman1+github@microsoft.com>
2025-12-11 11:19:03 +0000

    removing any prebuilt packages if present

--- package-deb.sh
+++ package-deb.sh
@@ -3,6 +3,7 @@ set -x
 
 RELEASENAME="$1"
 
+rm ../azfilesauth_*.deb || true
 sudo apt-get update
 sudo apt-get install -y git autoconf libtool build-essential python3 libcurl4-openssl-dev libkrb5-dev debhelper-compat
 sudo dpkg-buildpackage -us -uc
